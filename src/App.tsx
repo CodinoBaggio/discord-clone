950994fdbb05d6dd5a6148b49543b3d6a7a255b2
higashi <k-higashi@nihongas.co.jp>
2023-08-08 21:03:27 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import { Sidebar } from './components/sidebar/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Sidebar.scss';
+
+export const Sidebar = () => {
+  return (
+    <div>
+      {/* sidebarLeft */}
+      <div className="sidebarLeft"></div>
+      {/* sidebarRight */}
+      <div className="sidebarRight"></div>
+    </div>
+  );
+};
